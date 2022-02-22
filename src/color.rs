125ae551e97db5a6cee348052c54f37e6bf83573
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-22 16:41:09 +0100

    more colors

--- src/color.rs
+++ src/color.rs
@@ -1,12 +1,22 @@
 pub enum Color {
     GRID,
     BLANK,
+    START,
+    TARGET,
+    PATH,
+    OPEN_SET,
+    CLOSED_SET,
 }
 impl Color {
     pub fn get(value: Color) -> String {
         match value {
             Color::GRID => String::from("#000000"),
             Color::BLANK => String::from("#ffffff"),
+            Color::START => String::from("#00ff00"),
+            Color::TARGET => String::from("#00ff00"),
+            Color::PATH => String::from("#0000ff"),
+            Color::OPEN_SET => String::from("#00ff00"),
+            Color::CLOSED_SET => String::from("#ff0000"),
         }
     }
 }
