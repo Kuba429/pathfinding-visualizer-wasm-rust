74af2f7e800acd22600e894360a0f1b67d3ad572
Kuba429 <jakubzawadzki429@gmail.com>
2022-02-26 17:40:45 +0100

    naming

--- src/event_listeners/mod.rs
+++ src/event_listeners/mod.rs
@@ -1,11 +1,11 @@
-mod button_onclick;
-mod canvas;
+mod button_listeners;
+mod canvas_listeners;
 use crate::grid::Grid;
 use std::cell::RefCell;
 use std::rc::Rc;
 
 pub fn set_all_listeners(grid_ref: Rc<RefCell<Grid>>) {
-    button_onclick::set_start_button_onclick(grid_ref.clone());
-    button_onclick::set_random_walls_onclick(grid_ref.clone());
-    canvas::set_canvas_onclick(grid_ref.clone());
+    button_listeners::set_start_button_onclick(grid_ref.clone());
+    button_listeners::set_random_walls_onclick(grid_ref.clone());
+    canvas_listeners::set_canvas_onclick(grid_ref.clone());
 }
