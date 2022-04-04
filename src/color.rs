653301bf23d851bf2723c1d3ad15141b80645da3
Kuba429 <jakubzawadzki429@gmail.com>
2022-04-04 17:43:08 +0200

    color changes

--- src/color.rs
+++ src/color.rs
@@ -11,12 +11,12 @@ impl Color {
     pub fn get(value: Color) -> String {
         match value {
             Color::GRID => String::from("#000000"),
-            Color::BLANK => String::from("#ffffff"),
-            Color::START => String::from("#0000ff"),
-            Color::TARGET => String::from("#0000ff"),
-            Color::PATH => String::from("#0000ff"),
-            Color::OPEN_SET => String::from("#00ff00"),
-            Color::CLOSED_SET => String::from("#ff0000"),
+            Color::BLANK => String::from("#f7f5f2"),
+            Color::START => String::from("#077dff"),
+            Color::TARGET => String::from("#077dff"),
+            Color::PATH => String::from("#077dff"),
+            Color::OPEN_SET => String::from("#77D970"),
+            Color::CLOSED_SET => String::from("#E02401"),
         }
     }
 }
