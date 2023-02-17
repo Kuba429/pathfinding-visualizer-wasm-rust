55ea9637a2c5e9d2964ec5e96ab49097736088ac
Kuba429 <jakubzawadzki429@gmail.com>
2023-02-17 14:16:42 +0100

    inpt listeners

--- src/event_listeners/input_listeners.rs
+++ src/event_listeners/input_listeners.rs
@@ -1,31 +1,30 @@
 use crate::grid::{stage, Grid};
 use std::cell::RefCell;
 use std::rc::Rc;
-use stdweb::{
-    traits::IEvent,
-    unstable::TryInto,
-    web::{document, html_element::InputElement, IEventTarget, IParentNode},
-};
+use wasm_bindgen::{prelude::Closure, JsCast};
 
 pub fn set_size_range_oninput(grid_ref: Rc<RefCell<Grid>>) {
-    let input_element: InputElement = document()
+    let doc = web_sys::window().unwrap().document().unwrap();
+    let input_element: web_sys::HtmlInputElement = doc
         .query_selector("#gridSizeRange")
         .unwrap()
         .unwrap()
-        .try_into()
+        .dyn_into()
         .unwrap();
-    input_element.add_event_listener({
-        move |e: stdweb::web::event::InputEvent| {
-            let mut grid = grid_ref.borrow_mut();
-            match grid.stage {
-                stage::idle => {
-                    let target: InputElement = e.target().unwrap().try_into().unwrap();
-                    let value = target.raw_value().parse().unwrap();
-                    grid.rows = value;
-                    grid.reset();
-                }
-                _ => return,
+    let cb = Closure::<dyn FnMut(_)>::new(move |e: web_sys::InputEvent| {
+        let mut grid = grid_ref.borrow_mut();
+        match grid.stage {
+            stage::idle => {
+                let target: web_sys::HtmlInputElement = e.target().unwrap().dyn_into().unwrap();
+                let value: i32 = target.value().parse().unwrap();
+                grid.rows = value;
+                grid.reset();
             }
+            _ => return,
         }
     });
+    input_element
+        .add_event_listener_with_callback("input", cb.as_ref().unchecked_ref())
+        .unwrap();
+    cb.forget();
 }
